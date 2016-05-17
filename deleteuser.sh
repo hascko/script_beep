c0d21f6bdc12c8926395517fec748109cea8abf7
Hassane Sangare <sangare@intechinfo.fr>
2016-05-17 12:07:31 +0200

    correction

--- deleteuser.sh
+++ deleteuser.sh
@@ -15,18 +15,18 @@ if test -z $#;
                 
                 then
 	                #Recherche de l'utilisateur
-	                sed '/$1/d' /var/dialplan/$2.conf > /var/tmp/temp
+	                sed /$1/d /var/dialplan/$2.conf > /var/tmp/temp
 	                mv /var/tmp/temp /var/dialplan/$2.conf
 
 	                #Suppression de l'utilisateur
 	                rm /var/user/$1.conf
 
                     #Suppression dqns SIP
-                    sed '/$1/d' /etc/asterisk/sip.conf > /var/tmp/temp
+                    sed /$1/d /etc/asterisk/sip.conf > /var/tmp/temp
                     mv /var/tmp/temp /etc/asterisk/sip.conf
 
                     #Suppression dqns Voice;ail
-                    sed '/$1/d' /etc/asterisk/voicemail.conf > /var/tmp/temp
+                    sed /$1/d /etc/asterisk/voicemail.conf > /var/tmp/temp
                     mv /var/tmp/temp /etc/asterisk/voicemail.conf
 	            else
                     #Affichage du message
