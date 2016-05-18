6d5d8653b100529ea82c4d0c6567b3fe620c8d85
Hassane Sangare <sangare@intechinfo.fr>
2016-05-18 22:03:34 +0200

    correction script droit

--- cancel_droit_unidirectionnel.sh
+++ cancel_droit_unidirectionnel.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 
-user=$1
+context2=$1
 context=$2
 
-verif=`find /var/user/$user.conf 2>/dev/null`
-
-if [ -z "$verif" ];then
-        echo "$user n'existe pas"
-else
-        context2=`grep -n "context" "/var/user/$user.conf" | cut -d"=" -f2`
-        sed -i "/$context.conf/d" /var/dialplan/$context2.conf
-        sed -i "/$context/d" /var/dialplan/$context2.conf
-fi
+sed -i "/$context.conf/d" /var/dialplan/$context2.conf
+sed -i "/$context/d" /var/dialplan/$context2.conf
 
 asterisk -rx "dialplan reload"
