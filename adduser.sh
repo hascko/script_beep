949c5576cc5e037d85b740ef823401e2335ca632
Mougnin Serge Louis Adolphe <mougnin@intechinfo.fr>
2016-05-18 14:40:58 +0200

    Modification script ajout utilisateur mercredi 18

--- adduser.sh
+++ adduser.sh
@@ -6,6 +6,20 @@
 #Date 11/05/2016
 #---------------------------------------------------------------------
 
+---------------------------------------------------------------------
+#Arguments
+#
+#$1 : Le pseudo de l'utilisateur
+#$2 : Le mot de passe de l'utilisateur
+#$3 : L'adresse mail de l'utilisateur
+#$4 : L'extension (numéro) de l'utilisateur
+#$5 : Le protocole de l'utilisateur SIP ou DAHDI
+#$6 : Le port Dahdi
+#$7 : L'option de transfert
+#$8 : Le numéro de téléphone
+#$9 : Le contexte auquel on veut l'utilisateur
+#---------------------------------------------------------------------
+
 if test -z "$1";
         then
                 /bin/echo "Erreur ! Entrer les bons arguments"
@@ -13,30 +27,30 @@ if test -z "$1";
 
                 if [ $5 = 'SIP' ]
                         then
-                                if ( find /var/dialplan/$8.conf )
+                                if ( find /var/dialplan/$9.conf 2>/dev/null )
                                         then
                                                 #Ajout du dialplan
                                                 /bin/echo "Plan d'appels existants"
-                                                /bin/echo "exten => $4,1,Macro(travail,$1)" >> /var/dialplan/$8.conf
+                                                /bin/echo "exten => $4,1,Macro(travail,$1)" >> /var/dialplan/$9.conf
 
                                                 #Ajout du mail
-                                                /bin/echo "[$8]" >> /etc/asterisk/voicemail.conf
+                                                /bin/echo "[$9]" >> /etc/asterisk/voicemail.conf
                                                 /bin/echo "$4 => $2,$1,$3" >> /etc/asterisk/voicemail.conf
                                         else
                                                 #Creation du dialplan
-                                                /bin/echo "[$8]" >> /var/dialplan/$8.conf
-                                                /bin/echo "#include \"/var/dialplan/macro.conf\"" >> /var/dialplan/$8.conf
-                                                /bin/echo "include => macro-travail" >> /var/dialplan/$8.conf
-                                                /bin/echo " " >> /var/dialplan/$8.conf
-                                                /bin/echo "exten => $4,1,Macro(travail,$1)" >> /var/dialplan/$8.conf
-                                                /bin/echo "#include \"/var/dialplan/$8.conf\"" >> /etc/asterisk/extensions.conf
+                                                /bin/echo "[$9]" >> /var/dialplan/$9.conf
+                                                /bin/echo "#include \"/var/dialplan/macro.conf\"" >> /var/dialplan/$9.conf
+                                                /bin/echo "include => macro-travail" >> /var/dialplan/$9.conf
+                                                /bin/echo " " >> /var/dialplan/$9.conf
+                                                /bin/echo "exten => $4,1,Macro(travail,$1)" >> /var/dialplan/$9.conf
+                                                /bin/echo "#include \"/var/dialplan/$9.conf\"" >> /etc/asterisk/extensions.conf
 
                                                 #Ajout du mail
-                                                /bin/echo "[$8]" >> /etc/asterisk/voicemail.conf
+                                                /bin/echo "[$9]" >> /etc/asterisk/voicemail.conf
                                                 /bin/echo "$4 => $2,$1,$3" >> /etc/asterisk/voicemail.conf
                                 fi
 
-                                if ( find /var/user/$1.conf )
+                                if ( find /var/user/$1.conf 2>/dev/null )
                                         then
                                                 #Message pour indiquer le groupe
                                                 /bin/echo "Utilisateur existant"
@@ -45,7 +59,7 @@ if test -z "$1";
                                                 /bin/echo "[$1]" >> /var/user/$1.conf
                                                 /bin/echo "type=friend" >> /var/user/$1.conf
                                                 /bin/echo "secret=$2" >> /var/user/$1.conf
-                                                /bin/echo "context=$8" >> /var/user/$1.conf
+                                                /bin/echo "context=$9" >> /var/user/$1.conf
                                                 /bin/echo "username=$1" >> /var/user/$1.conf
                                                 /bin/echo "host=dynamic" >> /var/user/$1.conf
                                                 /bin/echo "#include \"/var/user/$1.conf\"" >> /etc/asterisk/sip.conf
@@ -53,30 +67,28 @@ if test -z "$1";
                         
                         elif [ $5 = 'DAHDI' ]
                                 then
-                                if ( find /var/dialplan/dahdi_$8.conf )
+                                if ( find /var/dialplan/dahdi_$9.conf 2>/dev/null )
                                         then
                                                 #Ajout du dialplan
                                                 /bin/echo "Plan d'appels existants"
-                                                /bin/echo "exten => $4,1,Dial(dahdi/2/30)" >> /var/dialplan/dahdi_$8.conf
+                                                /bin/echo "exten => $4,1,Dial(dahdi/2/30)" >> /var/dialplan/dahdi_$9.conf
 
                                                 #Ajout du mail
-                                                /bin/echo "[$8]" >> /etc/asterisk/voicemail.conf
+                                                /bin/echo "[$9]" >> /etc/asterisk/voicemail.conf
                                                 /bin/echo "$4 => $2,$1,$3" >> /etc/asterisk/voicemail.conf
                                         else
                                                 #Creation du dialplan
-                                                /bin/echo "[$8]" >> /var/dialplan/dahdi_$8.conf
-                                                /bin/echo " " >> /var/dialplan/dahdi_$8.conf
-                                                /bin/echo "exten => $4,1,Dial(dahdi,2,30)" >> /var/dialplan/dahdi_$8.conf
-                                                /bin/echo "#include \"/var/dialplan/dahdi_$8.conf\"" >> /etc/asterisk/extensions.conf
+                                                /bin/echo "[$9]" >> /var/dialplan/dahdi_$9.conf
+                                                /bin/echo " " >> /var/dialplan/dahdi_$9.conf
+                                                /bin/echo "exten => $4,1,Dial(dahdi/2/30)" >> /var/dialplan/dahdi_$9.conf
+                                                /bin/echo "#include \"/var/dialplan/dahdi_$9.conf\"" >> /etc/asterisk/extensions.conf
 
                                                 #Ajout du mail
-                                                /bin/echo "[$8]" >> /etc/asterisk/voicemail.conf
+                                                /bin/echo "[$9]" >> /etc/asterisk/voicemail.conf
                                                 /bin/echo "$4 => $2,$1,$3" >> /etc/asterisk/voicemail.conf
                                 fi
 
-                                chan1=`grep -h "channel1" "/var/user/dahdi_*.conf"`
-                                chan2=`grep -h "channel2" "/var/user/dahdi_*.conf"`
-                                chan3=`grep -h "channel3" "/var/user/dahdi_*.conf"`
+                                chan=`grep -h "channel=$6" "/var/user/dahdi_*.conf"`
 
                                 if ( find /var/user/dahdi_$1.conf )
                                         then
@@ -88,29 +100,15 @@ if test -z "$1";
                                                 /bin/echo "callerid=combinet <$4>" >> /var/user/dahdi_$1.conf
                                                 /bin/echo "echocancel=yes" >> /var/user/dahdi_$1.conf
 
-                                                if [ -z "$chan3" ];
+                                                if [ -z "$chan" ];
 
                                                         then 
-                                                                /bin/echo "group=3" >> /var/user/dahdi_$1.conf
-                                                                /bin/echo "channel=3" >> /var/user/dahdi_$1.conf
-
-                                                elif [ -z "$chan2" ];
-
-                                                        then
-                                                                /bin/echo "group=2" >> /var/user/dahdi_$1.conf
-                                                                /bin/echo "channel=2" >> /var/user/dahdi_$1.conf
-
-                                                elif [ -z "$chan1" ];
-
-                                                        then
-                                                                /bin/echo "group=1" >> /var/user/dahdi_$1.conf
-                                                                /bin/echo "channel=1" >> /var/user/dahdi_$1.conf
-
+                                                                /bin/echo "group=$6" >> /var/user/dahdi_$1.conf
+                                                                /bin/echo "channel=$6" >> /var/user/dahdi_$1.conf
                                                 else
-                                                                     /bin/echo "Plus de disponibilité pour Dahdi";
+                                                                     /bin/echo "Ce Port est occupé";
                                                 fi
-
-                                                #/bin/echo "#include \"/var/dialplan/dahdi_$1.conf\"" >> /etc/asterisk/chan_dahdi.conf
+												/bin/echo "#include \"/var/dialplan/dahdi_$1.conf\"" >> /etc/asterisk/chan_dahdi.conf
                                 fi
 
                         else
@@ -118,5 +116,6 @@ if test -z "$1";
                 fi
 fi
 
-#Redemarrage Asterisk
-/usr/sbin/service asterisk reload
+#Redemarrage des services asterisk
+asterisk -rx "dialplan reload"
+asterisk -rx "sip reload"
