079a4c3d7cbb1b0c4643e0b202a67641f8340fec
Hassane Sangare <sangare@intechinfo.fr>
2016-05-24 00:32:30 +0200

    correction script callcenter

--- droit_appel_bidirectionnel.sh
+++ droit_appel_bidirectionnel.sh
@@ -3,8 +3,8 @@
 context2=$1
 context=$2
 
-sed "1i#include /var/dialplan/$context.conf" /var/dialplan/$context2.conf > fichier.tmp && mv -f fichier.tmp /var/dialplan/$context2.conf; rm -f fichier.tmp
-sed "1i#include /var/dialplan/$context2.conf" /var/dialplan/$context.conf > fichier.tmp && mv -f fichier.tmp /var/dialplan/$context.conf; rm -f fichier.tmp
+sed "1i#include \"/var/dialplan/$context.conf\"" /var/dialplan/$context2.conf > fichier.tmp && mv -f fichier.tmp /var/dialplan/$context2.conf; rm -f fichier.tmp
+sed "1i#include \"/var/dialplan/$context2.conf\"" /var/dialplan/$context.conf > fichier.tmp && mv -f fichier.tmp /var/dialplan/$context.conf; rm -f fichier.tmp
 echo "include => $context2" >> /var/dialplan/$context.conf
 echo "include => $context" >> /var/dialplan/$context2.conf
 
--- droit_appel_unidirectionnel.sh
+++ droit_appel_unidirectionnel.sh
@@ -3,7 +3,7 @@
 context2=$1
 context=$2
 
-sed "1i#include /var/dialplan/$context.conf" /var/dialplan/$context2.conf > fichier.tmp && mv -f fichier.tmp /var/dialplan/$context2.conf; rm -f fichier.tmp
+sed "1i#include \"/var/dialplan/$context.conf\"" /var/dialplan/$context2.conf > fichier.tmp && mv -f fichier.tmp /var/dialplan/$context2.conf; rm -f fichier.tmp
 echo "include => $context" >> /var/dialplan/$context2.conf
 
 asterisk -rx "dialplan reload"
