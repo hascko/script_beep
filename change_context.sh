656eb7328a9a3b4fb8d80b1fab31b123640debf6
steephen king <steephen king>
2016-06-22 14:47:43 +0200

    22/06 14:47

--- change_context.sh
+++ change_context.sh
@@ -17,58 +17,61 @@
 #------------------------------------------------------------------------------------------------------------------------------------------
 
 if test -z $#;
-        then
-                /bin/echo "Erreur ! Entrer en argument 1, le nom du client, en 2 l'option de transfert (0,1), en 3 le nouveau groupe et en argument 4 le nouveau groupe"
-        else
-                if [ $5 = 'SIP' ]
-                        then
-                                if ( find /var/user/$1.conf 2>/dev/null )
-                                	then
-                                		#Creation d'un point de sauvegarde
-						                cp /var/user/$1.conf /var/user/.old/$1.conf
+	then
+			/bin/echo "Erreur ! Entrer en argument 1, le nom du client, en 2 l'option de transfert (0,1), en 3 le nouveau groupe et en argument 4 le nouveau groupe"
+	else
+		if [ $5 = 'SIP' ]
+			then
+				if ( find /var/user/$1.conf 2>/dev/null )
+					then
+						#Creation d'un point de sauvegarde
+						cp /var/user/$1.conf /var/user/.old/$1.conf
 
-						                #Suppression de la ligne de mot de passe
-						                sed '/secret/d' /var/user/$1.conf > /var/tmp/temp
-						                mv /var/tmp/temp /var/user/$1.conf
+						#Suppression de la ligne de mot de passe
+						sed '/secret/d' /var/user/$1.conf > /var/tmp/temp
+						mv /var/tmp/temp /var/user/$1.conf
 
-						                #Réécriture de la ligne
-						                sed "4i secret=$4" /var/user/$1.conf > /var/tmp/temp
-						                mv /var/tmp/temp /var/user/$1.conf
+						#Réécriture de la ligne
+						sed "4i secret=$4" /var/user/$1.conf > /var/tmp/temp
+						mv /var/tmp/temp /var/user/$1.conf
 
-						                #Suppression de la ligne du groupe
-						                sed '/context/d' /var/user/$1.conf > /var/tmp/temp
-						                mv /var/tmp/temp /var/user/$1.conf
+						#Suppression de la ligne du groupe
+						sed '/context/d' /var/user/$1.conf > /var/tmp/temp
+						mv /var/tmp/temp /var/user/$1.conf
 
-						                #Réécriture de la ligne
-						                sed "4i context=$3" /var/user/$1.conf > /var/tmp/temp
-						                mv /var/tmp/temp /var/user/$1.conf
-						            else
-						            	/bin/echo "Entrez un utilisateur existant"
+						#Réécriture de la ligne
+						sed "4i context=$3" /var/user/$1.conf > /var/tmp/temp
+						mv /var/tmp/temp /var/user/$1.conf
+				else
+						/bin/echo "Entrez un utilisateur existant"
+				fi
+		
+		elif [ $5 = 'DAHDI' ]
+			then
+				if ( find /var/user/dahdi_$1.conf )
+					then
+						#Creation d'un point de sauvegarde
+					cp /var/user/$1.conf /var/user/.old/$1.conf
 
-						    elif [ $5 = 'DAHDI' ]
-                                then
-                                	if ( find /var/user/dahdi_$1.conf )
-                                        then
-                                        	#Creation d'un point de sauvegarde
-						                cp /var/user/$1.conf /var/user/.old/$1.conf
+					#Suppression de la ligne de mot de passe
+					sed '/secret/d' /var/user/$1.conf > /var/tmp/temp
+					mv /var/tmp/temp /var/user/$1.conf
 
-						                #Suppression de la ligne de mot de passe
-						                sed '/secret/d' /var/user/$1.conf > /var/tmp/temp
-						                mv /var/tmp/temp /var/user/$1.conf
+					#Réécriture de la ligne
+					sed "4i secret=$4" /var/user/$1.conf > /var/tmp/temp
+					mv /var/tmp/temp /var/user/$1.conf
 
-						                #Réécriture de la ligne
-						                sed "4i secret=$4" /var/user/$1.conf > /var/tmp/temp
-						                mv /var/tmp/temp /var/user/$1.conf
+					#Suppression de la ligne du groupe
+					sed '/context/d' /var/user/$1.conf > /var/tmp/temp
+					mv /var/tmp/temp /var/user/$1.conf
 
-						                #Suppression de la ligne du groupe
-						                sed '/context/d' /var/user/$1.conf > /var/tmp/temp
-						                mv /var/tmp/temp /var/user/$1.conf
-
-						                #Réécriture de la ligne
-						                sed "4i context=$3" /var/user/$1.conf > /var/tmp/temp
-						                mv /var/tmp/temp /var/user/$1.conf
-						            else
-						            	/bin/echo "Entrez un utilisateur existant"
+					#Réécriture de la ligne
+					sed "4i context=$3" /var/user/$1.conf > /var/tmp/temp
+					mv /var/tmp/temp /var/user/$1.conf
+				else
+					/bin/echo "Entrez un utilisateur existant"
+				fi
+		fi
 fi
 
 #Redemarrage des services asterisk
