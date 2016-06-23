e7ba547ea1179fc9b9a57fd1aaf6d6f8a4d60f32
steephen king <steephen king>
2016-06-23 19:20:57 +0200

    23/06/ 19:21

--- droit_appel_unidirectionnel.sh
+++ droit_appel_unidirectionnel.sh
@@ -7,7 +7,7 @@ verif=`grep -w "\#include \"/var/dialplan/$context.conf\"" /var/dialplan/$contex
 
 #if [ -z "$verif" ];then
 	#sed "1i#include \"/var/dialplan/$context.conf\"" /var/dialplan/$context2.conf > fichier.tmp && mv -f fichier.tmp /var/dialplan/$context2.conf; rm -f fichier.tmp
-	echo "include => $context" >> /var/dialplan/$context2.conf
+	echo "include => $context " >> /var/dialplan/$context2.conf
 #fi
 
 asterisk -rx "dialplan reload"
