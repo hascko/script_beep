4682c9e727e3c7f19b426d731df5f73e8a8ee98c
steephen king <steephen king>
2016-06-23 09:22:15 +0200

    23/06 09:32

--- change_context.sh
+++ change_context.sh
@@ -17,8 +17,9 @@
 #$6 : Nom
 #$7 : Mail
 #------------------------------------------------------------------------------------------------------------------------------------------
+arg=$#
 
-if [$# -ne 7]
+if [$arg != 7]
 	then
 			/bin/echo "Erreur ! Entrer en argument 1, le nom du client, en 2 l'option de transfert (0,1), en 3 le nouveau groupe et en argument 4 le nouveau groupe"
 	else
