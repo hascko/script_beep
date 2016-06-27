a8677357a61758c7cfef453e16bf01582ef87a52
steephen king <steephen king>
2016-06-27 18:37:26 +0200

    ,nen en en e

--- edit_user.sh
+++ edit_user.sh
@@ -7,8 +7,17 @@ if test -z $#;
 				test=`grep -w ";----$1----" /var/dialplan/$2.conf`
 				sed -i -e "/;----$1----/d" /var/dialplan/$2.conf
 				echo $test >> /var/dialplan/default.conf
+				
+				#Suppression de la ligne du groupe
+				sed '/context/d' /var/user/$1.conf > /var/tmp/temp
+				mv /var/tmp/temp /var/user/$1.conf
+
+				#Réécriture de la ligne
+				sed "4i context=$3" /var/user/$1.conf > /var/tmp/temp
+				mv /var/tmp/temp /var/user/$1.conf
 
 fi
 
 #Redemarrage des services asterisk
 asterisk -rx "dialplan reload"
+asterisk -rx "sip reload"
