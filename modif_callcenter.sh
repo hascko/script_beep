f517e6bc6c9c4b7feb2caf02f2605e3fd737b729
Hassane Sangare <sangare@intechinfo.fr>
2016-07-01 12:13:22 +0200

    lmnvjfvnjfi

--- modif_callcenter.sh
+++ modif_callcenter.sh
@@ -3,8 +3,9 @@
 nom_call=$1
 new_strategy=$2
 
-verif=`grep -w -n "\[$nom_call\]" /etc/asterisk/queues.conf | cut -d":" -f1`
+verif=`grep -w -n "\[$nom_call\](StandardQueue)" /etc/asterisk/queues.conf | cut -d":" -f1`
 let verif++
 sed ""$verif"c strategy="$new_strategy"" /etc/asterisk/queues.conf > fichier.tmp && mv -f fichier.tmp /etc/asterisk/queues.conf; rm -f fichier.tmp
 
 asterisk -rx "module reload app_queue.so"
+asterisk -rx "reload"
