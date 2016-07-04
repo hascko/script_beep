b87b54b12df7396fedd6c4b1f515e85e4d07f9ec
MougninSLA <mougnin@intechinfo.fr>
2016-07-04 13:23:33 +0200

    super

--- standard_user.sh
+++ standard_user.sh
@@ -17,15 +17,15 @@ if test -z $#;
                 /bin/echo "Erreur ! Entrer en argument 1 et 2 l'heure et la minute de début, en argument 3 et 4 l'heure et la minute de fin et en 5 et 6 la plage de jours en anglais"
         else
                 #Suppression de la ligne de configuration
-                sed '/UTILISATEUR/d' /var/tmp/standard.conf > /var/tmp/temp
-                mv /var/tmp/temp /var/tmp/standard.conf
+                sed '/UTILISATEUR/d' /var/dialplan/standard.conf > /var/tmp/temp
+                mv /var/tmp/temp /var/dialplan/standard.conf
 
                 #Réécriture de la ligne avec nos parametres
-                sed "16i exten => 1011,1,Dial(SIP\/$1,15,tT)   ;----UTILISATEUR----;" /var/tmp/standard.conf > /var/tmp/temp
-                mv /var/tmp/temp /var/tmp/standard.conf
+                sed "16i exten => 1011,1,Dial(SIP\/$1,15,tT)   ;----UTILISATEUR----;" /var/dialplan/standard.conf > /var/tmp/temp
+                mv /var/tmp/temp /var/dialplan/standard.conf
 				
-				sed "17i exten => s,1,Dial(SIP\/$1,15,tT)      ;----UTILISATEUR----;" /var/tmp/standard.conf > /var/tmp/temp
-                mv /var/tmp/temp /var/tmp/standard.conf
+				sed "17i exten => s,1,Dial(SIP\/$1,15,tT)      ;----UTILISATEUR----;" /var/dialplan/standard.conf > /var/tmp/temp
+                mv /var/tmp/temp /var/dialplan/standard.conf
 fi
 
 #Redemarrage des services asterisk
