d92d2db3d021d9f97a5559e45f05f605b3b60866
MougninSLA <mougnin@intechinfo.fr>
2016-07-05 00:13:01 +0200

    super

--- standard_user.sh
+++ standard_user.sh
@@ -16,18 +16,18 @@ if test -z $#;
         then
                 /bin/echo "Erreur ! Entrer en argument 1 et 2 l'heure et la minute de début, en argument 3 et 4 l'heure et la minute de fin et en 5 et 6 la plage de jours en anglais"
         else
-                #Suppression de la ligne de configuration
-                sed '/UTILISATEUR/d' /var/dialplan/standard.conf > /var/tmp/temp
-                mv /var/tmp/temp /var/dialplan/standard.conf
+			#Suppression de la ligne de configuration
+			sed '/UTILISATEUR/d' /var/dialplan/standard.conf > /var/tmp/temp
+			mv /var/tmp/temp /var/dialplan/standard.conf
 
-                #Réécriture de la ligne avec nos parametres
-                #sed "16i exten => 1011,1,Dial(SIP\/$1,15,tT)   ;----UTILISATEUR----;" /var/dialplan/standard.conf > /var/tmp/temp
-                #mv /var/tmp/temp /var/dialplan/standard.conf
-				sed -i '/;----DEBUT3----;/a \exten => 1011,1,Dial(SIP\/'$1',15,tT)   ;----UTILISATEUR----;' /var/dialplan/standard.conf
-				
-				#sed "17i exten => s,1,Dial(SIP\/$1,15,tT)      ;----UTILISATEUR----;" /var/dialplan/standard.conf > /var/tmp/temp
-                #mv /var/tmp/temp /var/dialplan/standard.conf
-				sed -i '/;----FIN3----;/i \exten => s,1,Dial(SIP\/'$1',15,tT)      ;----UTILISATEUR----;' /var/dialplan/standard.conf
+			#Réécriture de la ligne avec nos parametres
+			#sed "16i exten => 1011,1,Dial(SIP\/$1,15,tT)   ;----UTILISATEUR----;" /var/dialplan/standard.conf > /var/tmp/temp
+			#mv /var/tmp/temp /var/dialplan/standard.conf
+			sed -i '/;----DEBUT3----;/a \exten => 1011,1,Dial(SIP\/'$1',15,tT)   ;----UTILISATEUR----;' /var/dialplan/standard.conf
+
+			#sed "17i exten => s,1,Dial(SIP\/$1,15,tT)      ;----UTILISATEUR----;" /var/dialplan/standard.conf > /var/tmp/temp
+			#mv /var/tmp/temp /var/dialplan/standard.conf
+			sed -i '/;----FIN3----;/i \exten => s,1,Dial(SIP\/'$1',15,tT)      ;----UTILISATEUR----;' /var/dialplan/standard.conf
 fi
 
 #Redemarrage des services asterisk
