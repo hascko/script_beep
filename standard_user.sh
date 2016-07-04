e0314bc26c735f91162499c948b06463e148accb
MougninSLA <mougnin@intechinfo.fr>
2016-07-04 11:51:32 +0200

    super

--- standard_user.sh
+++ standard_user.sh
@@ -16,7 +16,21 @@
 $ancien="SIP/$1";
 $nouveau="SIP/$2";
 
-sed -i 's/$1/$2/g' /var/tmp/standard.conf
+if test -z $#;
+        then
+                /bin/echo "Erreur ! Entrer en argument 1 et 2 l'heure et la minute de début, en argument 3 et 4 l'heure et la minute de fin et en 5 et 6 la plage de jours en anglais"
+        else
+                #Suppression de la ligne de configuration
+                sed '/UTILISATEUR$1/d' /var/tmp/standard.conf > /var/tmp/temp
+                mv /var/tmp/temp /var/tmp/standard.conf
+
+                #Réécriture de la ligne avec nos parametres
+                sed "16i exten => 1011,1,Dial(SIP/$2,15,tT)   ;----UTILISATEUR$2----;" /var/tmp/standard.conf > /var/tmp/temp
+                mv /var/tmp/temp /var/tmp/standard.conf
+				
+				sed "17i exten => s,1,Dial(SIP/$2,15,tT)      ;----UTILISATEUR$2----;" /var/tmp/standard.conf > /var/tmp/temp
+                mv /var/tmp/temp /var/tmp/standard.conf
+fi
 
 #Redemarrage des services asterisk
 asterisk -rx "dialplan reload"
